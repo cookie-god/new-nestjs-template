5bceac8796e4dc43369e15c4f3c7b059d8b0d97b
cookie-god <lion0193@gmail.com>
2025-01-09 18:02:34 +0900

    feat: env 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,16 @@ import { UsersModule } from './users/users.module';
 import { ServiceExceptionToHttpExceptionFilter } from './config/excpetion-filter';
 import { APP_FILTER } from '@nestjs/core';
 import { GlobalExceptionFilter } from './config/excpetion-filter/global.exception.filter';
+import { ConfigModule } from '@nestjs/config';
+import * as process from 'node:process';
 
 @Module({
-    imports: [UsersModule],
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: `src/env/.${process.env.NODE_ENV}.env`,
+            isGlobal: true,
+        })
+        , UsersModule],
     controllers: [AppController],
     providers: [
         AppService,
