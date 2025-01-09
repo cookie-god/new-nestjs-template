d529da0116d2d859276644912068821642ce076d
cookie-god <lion0193@gmail.com>
2025-01-09 12:26:51 +0900

    feat: Swagger 설정 완료

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ServiceExceptionToHttpExceptionFilter } from './config/excpetion-filter';
+import { setupSwagger } from './config/swagger/swagger.config';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useGlobalFilters(new ServiceExceptionToHttpExceptionFilter());
+    setupSwagger(app);
     await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
