7e5e6323b7ac0d737c1ba1eaa68cdea845e1680b
cookie-god <lion0193@gmail.com>
2025-01-17 11:22:54 +0900

    feat: 유저 아이디를 통해 유저 정보를 가져오는 함수 생성

--- src/auth/auth.repository.ts
+++ src/auth/auth.repository.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { use } from 'passport';
 import { UserInfo } from 'src/entity/user.entity';
 import { EntityManager, Repository } from 'typeorm';
 
@@ -9,6 +10,16 @@ export class AuthRepository {
     @InjectRepository(UserInfo)
     private readonly userRepository: Repository<UserInfo>,
   ) {}
+
+  async findUserById(id: number): Promise<UserInfo> | null {
+    const user: UserInfo | null = await this.userRepository.findOne({
+      where: {
+        id: id,
+        status: 'ACTIVE',
+      },
+    });
+    return user;
+  }
 
   async findUserBySnsId(
     snsId: number,
