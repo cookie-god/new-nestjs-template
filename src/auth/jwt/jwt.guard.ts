1eaaa82dcbd612f154d7955baa3027b0ab6eb256
cookie-god <lion0193@gmail.com>
2025-01-23 16:05:50 +0900

    feat: jwt 인증 로직 일부 변경 - 로그인 여부에 상관없이 둘다 조회 가능하도록 변경

--- src/auth/jwt/jwt.guard.ts
+++ src/auth/jwt/jwt.guard.ts
@@ -1,8 +1,5 @@
-import {
-  ExecutionContext,
-  Injectable,
-  UnauthorizedException,
-} from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 import {
   ExpiredJWTException,
@@ -13,6 +10,29 @@ import {
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
+  // 인증 우회 로직 추가
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> | any {
+    const request = context.switchToHttp().getRequest();
+    const isPublic = this.reflector.get<boolean>(
+      'isPublic',
+      context.getHandler(),
+    );
+    if (isPublic) {
+      const token = request.headers.authorization?.split(' ')[1];
+
+      // JWT 토큰이 없는 경우 인증 건너뜀
+      if (!token) {
+        console.log('Public route without JWT, skipping authentication');
+        return true;
+      }
+    }
+    return super.canActivate(context);
+  }
+
   handleRequest<TUser = any>(
     err: any,
     user: any,
