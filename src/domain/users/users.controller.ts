f96940aeb03133e57b685cfef619d6e06c396669
cookie-god <lion0193@gmail.com>
2025-02-06 15:03:14 +0900

    setting: 불필요한 파일 제거

--- src/domain/users/users.controller.ts
+++ src/domain/users/users.controller.ts
@@ -1,14 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 
 import { UsersService } from './users.service';
-import { PostUsersRequestDto } from '../auth/dto/request/post-users-request.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
-
-  @Post()
-  async postUsers(@Body() postUsersDto: PostUsersRequestDto) {
-    return this.usersService.createUsers(postUsersDto);
-  }
 }
--- src/domain/users/users.service.ts
+++ src/domain/users/users.service.ts
@@ -1,12 +1,4 @@
 import { Injectable } from '@nestjs/common';
 
-import { PostUsersRequestDto } from '../auth/dto/request/post-users-request.dto';
-import { NotExistUserException } from 'src/config/exception/service.exception';
-
 @Injectable()
-export class UsersService {
-  async createUsers(postUserDto: PostUsersRequestDto) {
-    throw NotExistUserException();
-    return postUserDto;
-  }
-}
+export class UsersService {}
