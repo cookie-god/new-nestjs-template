8fa57a36d509ab6b2be287def6e68bcd0bebf8ea
cookie-god <lion0193@gmail.com>
2025-05-25 14:37:29 +0900

    feat: 도커 파일 실행 가능하도록 수정

--- src/config/logger/logger.ts
+++ src/config/logger/logger.ts
@@ -1,5 +1,5 @@
 import * as winston from 'winston';
-import DailyRotateFile from 'winston-daily-rotate-file';
+import * as DailyRotateFile from 'winston-daily-rotate-file';
 
 const { combine, timestamp, json, colorize, printf } = winston.format;
 
