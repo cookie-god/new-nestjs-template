8ae3c27029c61ea6cbc82c2b567e1f8f6e59ff2c
cookie-god <lion0193@gmail.com>
2025-06-05 22:59:59 +0900

    feat: apm 도입

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,14 @@ import { AppModule } from './app.module';
 import { setupSwagger } from './config/swagger/swagger.config';
 import { GlobalExceptionFilter } from './config/excpetion-filter';
 import { CustomValidationPipe } from './pipe/custom-validation.pipe';
+import * as apm from 'elastic-apm-node';
 
 async function bootstrap() {
+  apm.start({
+    serviceName: process.env.ELASTIC_APM_SERVICE_NAME,
+    serverUrl: process.env.ELASTIC_APM_SERVER_URL,
+    environment: process.env.ELASTIC_APM_ENVIRONMENT,
+  });
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new CustomValidationPipe());
   app.useGlobalFilters(new GlobalExceptionFilter());
